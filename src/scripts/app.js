e7d0750d3f72907ba2eac148790874e74c683594
Henrique Melanda <hjdesigner23@gmail.com>
2018-07-19 22:54:20 -0300

    Changing the html from h2 to label in the select titles is the correct one. Created a file with with json with each sentence to assemble the role text. Finished script that mounts role text

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,27 @@
-// import Card from './components/card';
+import { day, month, colors } from './components/dateJson';
 import './app.scss';
 
-// const cardElement = document.querySelector('[data-js="card"]');
-// Card('Henrique Melanda', cardElement);
+const buttonDiscover = document.querySelector('[data-js="discover"]');
+const selectDay = document.querySelector('[data-js="selectDay"]');
+const selectMonth = document.querySelector('[data-js="selectMonth"]');
+const selectColors = document.querySelector('[data-js="selectColors"]');
+const title = document.querySelector('[data-js="title"]');
+const form = document.querySelector('[data-js="form"]');
+const button = document.querySelector('[data-js="button"]');
+const result = document.querySelector('[data-js="result"]');
+const resultTitle = document.querySelector('[data-js="result-title"]');
+
+buttonDiscover.addEventListener('click', () => {
+	const valueDay = selectDay.options[selectDay.selectedIndex].value;
+	const valueMonth = selectMonth.options[selectMonth.selectedIndex].value;
+	const valueColors = selectColors.options[selectColors.selectedIndex].value;
+	const textDay = day.filter(item => +valueDay === item.id + 1);
+	const textMonth = month.filter(item => +valueMonth === item.id + 1);
+	const textColors = colors.filter(item => +valueColors === item.id + 1);
+	title.classList.add('hide');
+	form.classList.add('hide');
+	button.classList.add('hide');
+	result.classList.add('show');
+
+	resultTitle.innerHTML = `Seu rolê aleatório é: ${textDay[0].text} ${textMonth[0].text} ${textColors[0].text}`;
+});
---
+++ src/scripts/components/dateJson/index.js
@@ -0,0 +1,210 @@
+export const day = [
+	{
+		id: 0,
+		text: 'Tocando atabaque',
+	},
+	{
+		id: 1,
+		text: 'Bebasso',
+	},
+	{
+		id: 2,
+		text: 'Dando uma azeitada nessa porra',
+	},
+	{
+		id: 3,
+		text: 'Estiloso',
+	},
+	{
+		id: 4,
+		text: 'Vestido um poncho',
+	},
+	{
+		id: 5,
+		text: 'Lutando kickboxing',
+	},
+	{
+		id: 6,
+		text: 'Vestindo um jaleco',
+	},
+	{
+		id: 7,
+		text: 'Dibrando',
+	},
+	{
+		id: 8,
+		text: 'Fazendo embaixadinha',
+	},
+	{
+		id: 9,
+		text: 'Cantando',
+	},
+	{
+		id: 10,
+		text: 'Dançando',
+	},
+	{
+		id: 11,
+		text: 'Fazendo gol',
+	},
+	{
+		id: 12,
+		text: 'Levantando a taça',
+	},
+	{
+		id: 13,
+		text: 'Lendo',
+	},
+	{
+		id: 14,
+		text: 'Fazendo hangloose',
+	},
+	{
+		id: 15,
+		text: 'Sorrindo',
+	},
+	{
+		id: 16,
+		text: 'Comemorando',
+	},
+	{
+		id: 17,
+		text: 'A vontade',
+	},
+	{
+		id: 18,
+		text: 'Tocando piano',
+	},
+	{
+		id: 19,
+		text: 'Andando a cavalo',
+	},
+	{
+		id: 20,
+		text: 'Fazendo parkour',
+	},
+	{
+		id: 21,
+		text: 'Criando una criptomoeda',
+	},
+	{
+		id: 22,
+		text: 'Jogando basquete',
+	},
+	{
+		id: 23,
+		text: 'Jogando minecraft',
+	},
+	{
+		id: 24,
+		text: 'Fazendo churrasco',
+	},
+	{
+		id: 25,
+		text: 'Quer namorar comigo?',
+	},
+	{
+		id: 26,
+		text: 'Assistindo chaves',
+	},
+	{
+		id: 27,
+		text: 'Escutando metallica',
+	},
+	{
+		id: 28,
+		text: 'Tocando pandeiro',
+	},
+	{
+		id: 29,
+		text: 'Confuso',
+	},
+	{
+		id: 30,
+		text: 'Destilando',
+	},
+];
+export const month = [
+	{
+		id: 0,
+		text: 'Na final da copa',
+	},
+	{
+		id: 1,
+		text: 'Na cozinha',
+	},
+	{
+		id: 2,
+		text: 'Na balada',
+	},
+	{
+		id: 3,
+		text: 'No aeroporto',
+	},
+	{
+		id: 4,
+		text: 'No laboratório',
+	},
+	{
+		id: 5,
+		text: 'No hospital',
+	},
+	{
+		id: 6,
+		text: 'Na webcam',
+	},
+	{
+		id: 7,
+		text: 'No sítio',
+	},
+	{
+		id: 8,
+		text: 'No apartamento',
+	},
+	{
+		id: 9,
+		text: 'Em campo',
+	},
+	{
+		id: 10,
+		text: 'No filme do Van Damme',
+	},
+	{
+		id: 11,
+		text: 'Em um clipe',
+	},
+];
+export const colors = [
+	{
+		id: 0,
+		text: 'Segurando um barquinho',
+	},
+	{
+		id: 1,
+		text: 'Com uma mulher nos ombros',
+	},
+	{
+		id: 2,
+		text: 'Com uma cantora russa',
+	},
+	{
+		id: 3,
+		text: 'Com a Pablo Vittar',
+	},
+	{
+		id: 4,
+		text: 'Com o Péricles',
+	},
+	{
+		id: 5,
+		text: 'Com amigos',
+	},
+	{
+		id: 6,
+		text: 'Com o lula',
+	},
+	{
+		id: 7,
+		text: 'Com desconhecidos',
+	},
+];
